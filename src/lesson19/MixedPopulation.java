d664250555ccdbb43a0177079acecf9a0c3a69f9
wryyyyyy135 <62267244+wryyyyyy135@users.noreply.github.com>
2020-04-09 00:07:58 +0800

    改就完啦奥利给

--- src/lesson19/MixedPopulation.java
+++ src/lesson19/MixedPopulation.java
@@ -5,26 +5,19 @@ public class MixedPopulation extends Population{
   int numEssential;
   int numSkeptic;
   int numFlier;
-<<<<<<< HEAD
   int numDoc;
 
   public MixedPopulation(int numShelterInPlace, int numEssential, int numSkeptic, int numFlier, int numDoc){
     super(numShelterInPlace + numEssential + numSkeptic + numFlier + numDoc);
-=======
-  int numDoctor;
 
   public MixedPopulation(int numShelterInPlace, int numEssential, int numSkeptic, int numFlier, int numDoctor){
     super(numShelterInPlace + numEssential + numSkeptic + numFlier + numDoctor);
->>>>>>> e9295ee5f5937a4db47af9741f9de1878a15f482
     this.numShelterInPlace = numShelterInPlace;
     this.numEssential = numEssential;
     this.numSkeptic = numSkeptic;
     this.numFlier = numFlier;
-<<<<<<< HEAD
     this.numDoc = numDoc;
-=======
-    this.numDoctor = numDoctor;
->>>>>>> e9295ee5f5937a4db47af9741f9de1878a15f482
+
 
   }
 
@@ -41,14 +34,8 @@ public class MixedPopulation extends Population{
     for(int i=0; i<this.numFlier; i++){
       this.addPerson(new FrequentFlier());
     }
-<<<<<<< HEAD
     for(int i=0; i<this.numDoc; i++){
       this.addPerson(new Doctor());
     }
-=======
-    for(int i=0; i<this.numDoctor; i++){
-        this.addPerson(new Doctor());
-      }
->>>>>>> e9295ee5f5937a4db47af9741f9de1878a15f482
   }
 }
