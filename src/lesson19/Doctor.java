c6fe00004cb0acbec3bae22942d117016e0f4193
wryyyyyy135 <62267244+wryyyyyy135@users.noreply.github.com>
2020-04-09 00:02:12 +0800

    小修改

--- src/lesson19/Doctor.java
+++ src/lesson19/Doctor.java
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 package lesson19;
-
-import java.util.ArrayList;
 
 public class Doctor extends Person {
 	private double cureProb=0.3;
@@ -31,46 +28,4 @@ public class Doctor extends Person {
 
 	}
 }
-=======
-package lesson19;
 
-import java.util.ArrayList;
-
-public class Doctor extends Person {
-
-	public Doctor(int x, int y, Country country) {
-		super(x, y, country);
-		// TODO Auto-generated constructor stub
-	}
-
-	public Doctor() {
-		super();
-	}
-
-	void tryToMove() {
-		super.tryToMoveRandomly();
-		treatment();
-	}
-	
-	/*
-	 * the doctor can treat one person around him
-	 */
-	public void treatment(){
-		ArrayList<Person> potentialPatients = new ArrayList<Person>();
-		for(int i = -1; i <=1 ; i++) {
-			for (int j = -1; j <= 1; j++) {
-				if(country.places[x-i][y] != null && country.places[x-1][y].infected == true && !(i==0&&j==0)) {
-					potentialPatients.add(country.places[x-i][y]);
-				}
-			}
-		}
-		if(potentialPatients != null) {
-			int ranNum = (int) (Math.random()*(potentialPatients.size()+1));
-			potentialPatients.get(ranNum).recovered = true;
-			System.out.println("treated");
-		}
-			
-	}
-
-}
->>>>>>> ed60f3fe5c629a3c722fc4af7339489e57965284
