6c0f8484c878fe0e7e37847bd529d28b93e752d1
longyi1207 <59838570+longyi1207@users.noreply.github.com>
2020-04-09 21:53:52 -0400

    test

--- src/lesson19/AnalyzeSimulation.java
+++ src/lesson19/AnalyzeSimulation.java
@@ -1,14 +1,23 @@
 package lesson19;
 import java.util.Scanner;
-public class AnalyzeSimulation{
+public class AnalyzeSimulation extends RunSimulation{
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		String arg = in.nextLine();
-		String[] ar = arg.split(" ");
-		int trial = 0;
-		while (trial <= 100) {
-			RunSimulation.main(ar);
-			
+		// first we get the simulation parameters
+		// from the command line
+		int width = Integer.parseInt(args[0]);
+		int height = Integer.parseInt(args[1]);
+		int numStayHome = Integer.parseInt(args[2]);
+		int numEssential = Integer.parseInt(args[3]);
+		int numSkeptic = Integer.parseInt(args[4]);
+		int numFlier = Integer.parseInt(args[5]);
+		int numDoc = Integer.parseInt(args[6]);
+		int repetition=Integer.parseInt(args[7]);
+		int sumDays=0;
+		int sumInfected=0;
+		int peakInfected=0;
+		for(int i=0;i<repetition;i++) {
+			super.(width,height,numStayHome,numEssential,numSkeptic,numFlier,numDoc,repetition);
+					
 		}
 	}
 }
