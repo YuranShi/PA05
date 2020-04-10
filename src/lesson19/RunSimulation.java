8136370d2f3a76f7ae54ac90e7ce6b1c98004e6a
wryyyyyy135 <62267244+wryyyyyy135@users.noreply.github.com>
2020-04-10 10:45:19 +0800

    除了numinfected都是对的

--- src/lesson19/RunSimulation.java
+++ src/lesson19/RunSimulation.java
@@ -12,27 +12,14 @@ public class RunSimulation {
 	private static int MAX_TICKS=1000;
 
 	private Random random = new Random();
-
+	
 	public int[] run(int width,int height,int numStayHome,int numEssential,int numSkeptic,int numFlier,int numDoc,int repetition) {
 		// first we get the simulation parameters
 		// from the command line
 
-//		int width = Integer.parseInt(args[0]);
-//		int height = Integer.parseInt(args[1]);
-//		int numStayHome = Integer.parseInt(args[2]);
-//		int numEssential = Integer.parseInt(args[3]);
-//		int numSkeptic = Integer.parseInt(args[4]);
-//		int numFlier = Integer.parseInt(args[5]);
-//		int numDoc = Integer.parseInt(args[6]);
 
 		// next we create the population and the country
 		Population population;
-
-		//population = new Population(numPeople);
-		//population = new AllStayAtHome(numPeople);
-		//int numEssential = numPeople/10;
-		//int numOther = numPeople/20;
-		//int numStayHome = numPeople - numEssential - numOther;
 
 		population = new MixedPopulation(numStayHome, numEssential, numSkeptic, numFlier,numDoc);
 		population.createPeople();
@@ -60,21 +47,40 @@ public class RunSimulation {
 		
 		int[] result= new int[]{ticks,numInfected,peakInfected};
 		return result;
+		
+	}
+	public static void main(String[] args) {
+		int width = Integer.parseInt(args[0]);
+		int height = Integer.parseInt(args[1]);
+		int numStayHome = Integer.parseInt(args[2]);
+		int numEssential = Integer.parseInt(args[3]);
+		int numSkeptic = Integer.parseInt(args[4]);
+		int numFlier = Integer.parseInt(args[5]);
+		int numDoc = Integer.parseInt(args[6]);
+		
+		Population population;
+		
+		population = new MixedPopulation(numStayHome, numEssential, numSkeptic, numFlier,numDoc);
+		population.createPeople();
+		Country country = new Country(width,height);
+		// and add a link to the population in the country
+		country.population = population;
+		// next we place the people into the country randomly
+		population.placePeople(country);
+		System.out.println("Initial State of the Country");
+		country.printCountry();
 
-//		System.out.println("Initial State of the Country");
-//		country.printCountry();
-//
-//		System.out.println("\nTracking the Infection");
-//		for(int k=0;k<MAX_TICKS; k++) {
-//			country.simulateOneStep();
-//			country.printState(k);
-//
-//			if (country.numInfected==0) {
-//				break;
-//
-//			}
-//		}
-//		System.out.println("\nFinal State of the Country");
-//		country.printCountry();
+		System.out.println("\nTracking the Infection");
+		for(int k=0;k<MAX_TICKS; k++) {
+			country.simulateOneStep();
+			country.printState(k);
+
+			if (country.numInfected==0) {
+				break;
+
+			}
+		}
+		System.out.println("\nFinal State of the Country");
+		country.printCountry();
 	}
 }
